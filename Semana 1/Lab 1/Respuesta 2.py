eabde8959902ffaaa14c1eae07dd9a30dd2d0c1b
Tpz <pzthelma@gmail.com>
2025-01-26 18:21:25 -0600

    Create Ejercicio 3.py

--- Semana 1/Lab 1/Respuesta 2.py
+++ Semana 1/Lab 1/Respuesta 2.py
@@ -1,5 +1,5 @@
 #Ejercicio 2
 
-#Respuesta
+#Respuesta:
 respuesta = """ un programa es un conjunto de instrucciones
 que se le dice a una computadora para que realice una accion determinada."""
---
+++ Semana 1/Lab 1/Respuesta 3.py
@@ -0,0 +1,6 @@
+#Ejercicio 3
+
+#Respuesta: 
+respuesta = """El intérprete ejecuta un código escrito
+en un lenguaje de programación,mientras tanto un compilador traduce un código
+de este lenguaje a código máquina"""
