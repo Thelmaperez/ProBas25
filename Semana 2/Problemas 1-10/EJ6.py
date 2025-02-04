e9931b831a563889f11550985d2a3bff5a571ca0
Tpz <pzthelma@gmail.com>
2025-02-03 18:44:20 -0600

    EJ6

--- Semana 2/Problemas 1-10/EJ6.py
+++ Semana 2/Problemas 1-10/EJ6.py
@@ -1,5 +1,19 @@
 #Problema 6
 
-print("calcular el interés compuesto")
+print("Calcular interés compuesto")
+
+
+def interes_compuesto(P, r, n, t):
+    A = P * (1 + r/n)**(n*t)  
+    return A
+
+capital_inicial = float(input("Introduce el capital inicial:"))
+tasa_interes = float(input("Introduce la tasa de interés anual (en porcentapor ejemplo, 4 para 4%): ")) / 100
+capitalizacion_anual = int(input("Introduce el número de veces que se capitaliza al año (por ejemplo, 1 para anual): "))
+tiempo = float(input("Introduce el tiempo en años: "))
+
+monto_final = interes_compuesto(capital_inicial, tasa_interes, capitalizacion_anual, tiempo)
+
+print(f"\nEl monto final después de {tiempo} años es: {monto_final:.2f}")
 
 
