1d62ac8a8c8a76c241554e5e118e38bbd0370537
Tpz <pzthelma@gmail.com>
2025-02-15 00:41:03 -0600

    P11

--- Semana 3/P11.py
+++ Semana 3/P11.py
@@ -1,7 +1,11 @@
-def es_palindromo(texto):
+#Problema 11
 
+def es_palindromo(texto):
     texto = texto.lower().replace(" ", "")
     return texto == texto[::-1]
 
 entrada = input("Ingresa una palabra o frase: ")
-print("Es un palíndromo." if es_palindromo(entrada) else "No es un palíndromo.")
+if es_palindromo(entrada):
+    print("Es un palíndromo.")
+else:
+    print("No es un palíndromo.")
