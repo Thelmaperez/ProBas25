90a0bc0b7631c18a38fc752a5c47f4dc4bb70a7d
Tpz <pzthelma@gmail.com>
2025-02-15 21:39:10 -0600

    P19

--- Semana 3/P19.py
+++ Semana 3/P19.py
@@ -1,3 +1,14 @@
 #Problema 19
+import numpy as np
 
+a, b = 0, 10
+uniforme = np.random.uniform(a, b, 10)  
+mu, sigma = 0, 1
+normal = np.random.normal(mu, sigma, 10)  
 
+n, p = 10, 0.5
+binomial = np.random.binomial(n, p, 10)  
+
+print("Uniforme:", uniforme)
+print("Normal:", normal)
+print("Binomial:", binomial)
