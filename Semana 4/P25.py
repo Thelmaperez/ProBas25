dd67a627a1fa5b92bec5e51fa0600c6837af3b9a
Tpz <pzthelma@gmail.com>
2025-02-22 21:51:23 -0600

    Create

--- Semana 4/P25.py
+++ Semana 4/P25.py
@@ -1,1 +1,20 @@
 #Problema 25
+#Generar y analizar histogramas de datos
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+data = np.random.normal(loc=50, scale=15, size=1000)  # Media=50, Desviación=15, 1000 datos
+
+
+plt.figure(figsize=(8, 5))
+sns.histplot(data, bins=20, kde=True, color='blue', edgecolor='black', alpha=0.7)
+
+plt.xlabel('Valor')
+plt.ylabel('Frecuencia')
+plt.title('Histograma de Datos con Curva de Densidad')
+
+
+plt.show()
