f065e632f1032647cb3df096b30b655b77c3f90d
Tpz <pzthelma@gmail.com>
2025-02-22 21:41:52 -0600

    create

--- Semana 4/P23.py
+++ Semana 4/P23.py
@@ -1,1 +1,11 @@
 #Problema 23
+#Implementar y operar con matrices
+
+import numpy as np
+
+def mostrar(matriz):
+    print(matriz.shape)
+    print(type(matriz))
+    print(matriz)
+
+    mostrar(np.array([[23,12,3],[2,67,23],[56,74,2]]))
