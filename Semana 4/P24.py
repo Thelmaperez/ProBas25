e5fcc7baf6b9c481e7a46fc75806be05b9c74ec0
Tpz <pzthelma@gmail.com>
2025-02-22 21:46:34 -0600

    Create

--- Semana 4/P24.py
+++ Semana 4/P24.py
@@ -1,1 +1,11 @@
 #Problema 24
+#Calcular la suma de una serie numérica.
+
+def sum_series(n, formula):
+ 
+    return sum(formula(i) for i in range(1, n + 1))
+
+
+formula = lambda i: 1 / i
+resultado = sum_series(n , formula)
+print(f"La suma de la serie es: {resultado}")
